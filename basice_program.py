bfbba7e120dd1561ce4e76f2067b4ac3bfff5816
Keerthanareddy2003 <128774075+Keerthanareddy2003@users.noreply.github.com>
2024-10-28 22:09:40 +0530

    Add files via upload

---
+++ basice_program.py
@@ -0,0 +1,27 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=4
+>>> x
+4
+>>> 4
+4
+>>> y=5.4
+>>> y
+5.4
+>>> x='keerthana'
+>>> x
+'keerthana'
+>>> y=true
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    y=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+>>> y=True
+>>> y
+True
+>>> i=100
+>>> y
+True
+>>> i=100
+>>> i
+100
